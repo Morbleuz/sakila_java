b090d015df4b9aca1085399f3a2e7f188ddf86c4
Axel Laude <axel2003@outlook.com>
2024-04-12 15:04:19 +0200

    Ajout de la route pour avoir un seul country

--- src/main/java/epsi/software/sakila/controllers/CountryController.java
+++ src/main/java/epsi/software/sakila/controllers/CountryController.java
@@ -7,14 +7,11 @@ import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
-@RequestMapping("/countries")
 public class CountryController {
     private CountryService service;
     private static final Logger log = LoggerFactory.getLogger(CountryController.class);
@@ -22,14 +19,14 @@ public class CountryController {
         this.service = service;
     }
 
-    @GetMapping(path = "getAll")
+    @GetMapping("/countries/")
     public List<Country> getAll(){
         log.info("Call get all country");
         return this.service.readAll();
     }
-    @GetMapping(path = "getOne")
-    public Country getOneCountry(@RequestParam String id) {
-        Country country = this.service.read(Long.parseLong(id));
+    @GetMapping("/countries/{id}")
+    public Country getOneCountry(@PathVariable Long id) {
+        Country country = this.service.read(id);
         return country;
     }
 }
